f8ce33fedcf1079621caa10e3250bf3c7cfb8233
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-20 13:56:40 -0300

    Flipping cells

--- src/Board.js
+++ src/Board.js
@@ -71,12 +71,20 @@ class Board extends Component {
       }
     }
 
+    flipCell(y, x)
+    flipCell(y, x-1)
+    flipCell(y, x+1)
+    flipCell(y-1, x)
+    flipCell(y+1, x)
+
+
     // TODO: flip this cell and the cells around it
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon = false;
 
-    // this.setState({board, hasWon});
+    this.setState({board, hasWon});
   }
 
 
@@ -88,7 +96,7 @@ class Board extends Component {
       let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
         let coord = `${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />)
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAround={() => this.flipCellsAround(coord)} />)
       }
       tableBoard.push(<tr key={`row-${y}`}>{row}</tr>)
     }
